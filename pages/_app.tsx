e779ecdcbbcebfc186df6f9af0369ea278611d52
Tran Huy <huy.tq@positivethinking.tech>
2022-12-11 12:27:00 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
+import { Web3ReactProvider } from '@web3-react/core'
+import Web3 from 'web3'
+
+function getLibrary(provider: any) {
+  return new Web3(provider)
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  (
+  <Web3ReactProvider getLibrary={getLibrary}>
+    <Component {...pageProps} />
+  </Web3ReactProvider>
+  )
 }
